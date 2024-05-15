760dc52e3262d3ffa7e561bfa0fabb062e37b7db
keyaru77 <whyudacok@gmail.com>
2024-05-16 02:05:08 +0700

    Update parser.ts

--- src/source/kuramanime/parser.ts
+++ src/source/kuramanime/parser.ts
@@ -8,7 +8,7 @@ import type {
   ListAnime,
 } from "../utils/types";
 
-const BASEURL = "https://kuramanime.net";
+const BASEURL = "https://kuramanime.fun";
 
 export const recentRelease = async (page: number = 1): Promise<ListAnime> => {
   let list: Anime[] = [];
